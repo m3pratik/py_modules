043d6de732392a687a307a08be72d3c1ee7ca0e1
Pratik Panchal <m3pratik@gmail.com>
2022-03-23 23:04:07 +0530

    Second Program

---
+++ file2.py
@@ -0,0 +1,10 @@
+# Writing data to Text files.
+
+file1 = open("namespace_list.txt","w")
+
+file1.write('\nDelhi')
+file1.write('\nMumbai')
+file1.write('\nAWS')
+file1.write('\nGCP')
+
+file1.close()
